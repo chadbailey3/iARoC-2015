fd36a707346b59625656794802c2186c766f8cbc
chadbailey3 <chadbailey3@gmail.com>
2015-06-19 18:32:22 -0700

    We did stuff

--- src/org/jointheleague/iaroc/Brain.java
+++ src/org/jointheleague/iaroc/Brain.java
@@ -1,5 +1,5 @@
 package org.jointheleague.iaroc;
-
+//andrew is gre
 import android.os.SystemClock;
 
 import ioio.lib.api.IOIO;
@@ -20,7 +20,7 @@ public class Brain extends IRobotCreateAdapter {
         this.dashboard = dashboard;
     }
 
-    /* This method is executed when the robot first starts up. */
+    /* Thaaaaaaaaaaaaaz2qq  is method is executed when the robot first starts up. */
     public void initialize() throws ConnectionLostException {
         dashboard.log("Hello! I'm a Clever Robot!");
         //what would you like me to do, Clever Human?
@@ -29,17 +29,8 @@ public class Brain extends IRobotCreateAdapter {
     /* This method is called repeatedly. */
     public void loop() throws ConnectionLostException {
 //Gold Rush Code
-        readSensors(SENSORS_INFRARED_BYTE);
-        dashboard.log(getInfraredByte() + "");
-        if (getInfraredByte() == 248) {
-            driveDirect(500, 500);
-        } else if (getInfraredByte() == 244) {
-            driveDirect(500, 500);
-        } else if (getInfraredByte() == 255 || getInfraredByte() == 240) {
-            driveDirect(-100, 100);
-        } else {
-            driveDirect(0, 0);
-        }
+
+
 // Maze Runner Code
 //        readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
 //        driveDirect(175, 500);
@@ -50,6 +41,40 @@ public class Brain extends IRobotCreateAdapter {
 //            driveDirect(500, -500);
 //            SystemClock.sleep(350);
 //        }
+findBeacon();
+    }
+    public void goldRushCode(){
+        readSensors(SENSORS_INFRARED_BYTE);
+//        dashboard.log(getInfraredByte() + "");
+//        if (getInfraredByte() == 248) {
+//            driveDirect(500, 500);
+//        } else if (getInfraredByte() == 244) {
+//            driveDirect(500, 500);
+//        } else if (getInfraredByte() == 255 || getInfraredByte() == 240) {
+//            driveDirect(-100, 100);
+//        }
+//        else if (getInfraredByte() == 242) {
+//            driveDirect(500, 500);
+//        }
+//        else if (getInfraredByte() == 246) {
+//            driveDirect(500, 500);
+//        }
+//        else if (getInfraredByte() == 250) {
+//            driveDirect(500, 500);
+//        }
+//        else {
+//            driveDirect(0, 0);
+//        }
+    }
+    public void findBeacon(){
+        try {
+            driveDirect(-250, 250);
+            SystemClock.sleep(3250);
+            driveDirect(0, 0);
+            SystemClock.sleep(2000);
+        } catch (ConnectionLostException e) {
+            e.printStackTrace();
+        }
 
     }
 }
